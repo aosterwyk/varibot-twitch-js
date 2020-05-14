ac8ea8cc53cc4224d1758ed75fb4d331919ab943
Aaron <varixx@gmail.com>
2020-05-13 21:03:13 -0500

    fixed require paths

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const soundPlayer = require('play-sound')(opts = {player: 'mplayer.exe'});
 const tmi = require('tmi.js');
-const pubsubBot = require('../utils/pubsub');
-const botSettings = require('../botSettings.json');
+const pubsubBot = require('./utils/pubsub');
+const botSettings = require('./botSettings.json');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const { getRandomOwnedGame } = require('../utils/ownedGames');
-const twitchAPI = require('../utils/api');
+const { getRandomOwnedGame } = require('./utils/ownedGames');
+const twitchAPI = require('./utils/api');
 const chalk = require('chalk');
 
 const options = {
