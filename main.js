c3c4b0597b1efc6b71885e636a737e080c2dd5d2
Aaron <varixx@gmail.com>
2020-06-20 19:55:44 -0500

    changed default size

--- main.js
+++ main.js
@@ -413,14 +413,14 @@ var win = null;
 function createWindow() {
     win = new BrowserWindow({
         width: 1200,
-        height: 1200,
+        height: 800,
         webPreferences: {
             nodeIntegration: true
         }
     });
 
     win.loadFile('index.htm');
-
+    win.setMenu(null);
     // win.webContents.openDevTools()
     // win.webContents.executeJavaScript(`updateSoundsList()`);
     // win.webContents.executeJavaScript(`showPage('home')`);
