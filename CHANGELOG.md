3d7333f9b60f3b8867c7c1c39955f0a89af77f39
VariXx <varixx@gmail.com>
2020-09-03 21:47:21 -0500

    added invalid token error message when connecting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 - Fixed stream marker command 
+- Bot will give an error message when connecting if token is invalid
 
 ### Removed
 - Removed reloading sounds when switching to dashboard
--- main.js
+++ main.js
@@ -274,8 +274,8 @@ async function startBot() {
         }
 
         if(botSettings.token === undefined || botSettings.token.length < 1) { 
-            console.log('Invalid auth token. Please use the link below to authorize the bot and get a token.');
-            console.log(`https://id.twitch.tv/oauth2/authorize?client_id=${botSettings.clientId}&redirect_uri=https://acceptdefaults.com/twitch-oauth-token-generator/&response_type=token&scope=bits:read+channel:read:redemptions+channel:moderate+chat:edit+chat:read+user:edit:broadcast`);
+            // console.log('Invalid auth token. Please use the link below to authorize the bot and get a token.');
+            // console.log(`https://id.twitch.tv/oauth2/authorize?client_id=${botSettings.clientId}&redirect_uri=https://acceptdefaults.com/twitch-oauth-token-generator/&response_type=token&scope=bits:read+channel:read:redemptions+channel:moderate+chat:edit+chat:read+user:edit:broadcast`);
             readyToConnect = false;
             statusMsg(`error`,`Invalid bot settings. Please run setup.`);        
         } 
@@ -302,7 +302,19 @@ async function startBot() {
             channels: [botSettings.channel]
         }; 
         client = new tmi.client(options);    
-        client.connect();
+        client.connect()
+        .catch((error) => {
+            if(error.includes('Login authentication failed')) {
+                let errorString = `Invalid token. Please get a new token and update bot settings.`;
+                statusMsg('error', errorString);
+                win.webContents.executeJavaScript(`showPage('settings')`);
+                win.webContents.executeJavaScript(`alertMsg(true, 'error', '${errorString}')`);        
+            }
+            else {
+                statusMsg('error', `Error connecting: ${error}`);
+            }
+            return;
+        });
         client.on('connected', (address, port) => {
             console.log(`Chatbot (${options.identity.username}) connected to ${address}:${port}`);
         });
@@ -321,6 +333,30 @@ async function startBot() {
                 }
             }
         });    
+
+        pubsubSocket.onopen = async function(e) {
+            await botSettingsDB.sync();
+            let botset = await botSettingsDB.findOrCreate({where: {id: 1}}); 
+            botSettings = botset[0];  
+            if(botSettings !== undefined) {
+                try {
+                    let channelId = await twitchAPI.getChannelID(botSettings.channel, botSettings.clientId, botSettings.token);
+                    let connectMsg =  {
+                        type: "LISTEN",
+                        nonce: "44h1k13746815ab1r2",
+                        data:  {
+                        topics: ["channel-points-channel-v1." + channelId],
+                        auth_token: botSettings.token
+                        }
+                    };
+                    pubsubSocket.send(JSON.stringify(connectMsg));
+                    console.log(`Pubsub connected. Listed topics: ${connectMsg.data.topics}`);
+                    pubsubPings();
+                }
+                catch(error) {console.log(error);}
+            }
+        };
+        
         win.webContents.executeJavaScript(`updateSoundsList()`);
         win.webContents.executeJavaScript(`showPage('home')`);        
     }
@@ -601,26 +637,26 @@ function pubsubPings() {
     setTimeout(pubsubPings,120000); // 2 minutes
 }
 
-pubsubSocket.onopen = async function(e) {
-    await botSettingsDB.sync();
-    let botset = await botSettingsDB.findOrCreate({where: {id: 1}}); 
-    botSettings = botset[0];  
-    // TO DO - move bot settings to a command or load it all before starting these 
-    if(botSettings !== undefined) {
-        let channelId = await twitchAPI.getChannelID(botSettings.channel, botSettings.clientId, botSettings.token);
-        let connectMsg =  {
-            type: "LISTEN",
-            nonce: "44h1k13746815ab1r2",
-            data:  {
-            topics: ["channel-points-channel-v1." + channelId],
-            auth_token: botSettings.token
-            }
-        };
-        pubsubSocket.send(JSON.stringify(connectMsg));
-        console.log(`Pubsub connected. Listed topics: ${connectMsg.data.topics}`);
-        pubsubPings();
-    }
-};
+// pubsubSocket.onopen = async function(e) {
+//     await botSettingsDB.sync();
+//     let botset = await botSettingsDB.findOrCreate({where: {id: 1}}); 
+//     botSettings = botset[0];  
+//     // TO DO - move bot settings to a command or load it all before starting these 
+//     if(botSettings !== undefined) {
+//         let channelId = await twitchAPI.getChannelID(botSettings.channel, botSettings.clientId, botSettings.token);
+//         let connectMsg =  {
+//             type: "LISTEN",
+//             nonce: "44h1k13746815ab1r2",
+//             data:  {
+//             topics: ["channel-points-channel-v1." + channelId],
+//             auth_token: botSettings.token
+//             }
+//         };
+//         pubsubSocket.send(JSON.stringify(connectMsg));
+//         console.log(`Pubsub connected. Listed topics: ${connectMsg.data.topics}`);
+//         pubsubPings();
+//     }
+// };
 
 pubsubSocket.onmessage = function(event)  {
     pubsubResonse = JSON.parse(event.data);
