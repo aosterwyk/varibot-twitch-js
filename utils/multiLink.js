f06f2e8df0168f4124da8afeb7efcb986a5b0740
VariXx <VariXx@gmail.com>
2021-09-06 16:04:52 -0500

    added try when checking for stream title

--- utils/multiLink.js
+++ utils/multiLink.js
@@ -3,24 +3,29 @@ const { getChannelID, getStreamTitle } = require('./api.js');
 async function getMultiLink(channel, clientId, token) {
     let channelId = await getChannelID(channel, clientId, token);
     // let channelTitle = await getStreamTitle(channelId, clientId, token); //v5
-    let channelTitle = await getStreamInfo(channelId, clientId, token).title; //v6
-    let multiLink = `https://multistre.am/${channel}/`
-    if(channelTitle.includes('!multi') && channelTitle.includes('@')) { 
-        let mentionLocation = channelTitle.search('@');
-        if(mentionLocation != -1) {
-            let multiChannels = channelTitle.slice(mentionLocation);
-            multiChannels = multiChannels.trim().split(' ');
-            multiChannels.forEach((chan, c) => {
-            if(chan.includes('@') && chan.length > 4) {
-                multiLink += `${(chan.slice(1)).trim()}/`;
+    try {
+        let channelTitle = await getStreamInfo(channelId, clientId, token).title; //v6
+        let multiLink = `https://multistre.am/${channel}/`
+        if(channelTitle.includes('!multi') && channelTitle.includes('@')) { 
+            let mentionLocation = channelTitle.search('@');
+            if(mentionLocation != -1) {
+                let multiChannels = channelTitle.slice(mentionLocation);
+                multiChannels = multiChannels.trim().split(' ');
+                multiChannels.forEach((chan, c) => {
+                if(chan.includes('@') && chan.length > 4) {
+                    multiLink += `${(chan.slice(1)).trim()}/`;
+                }
+                });
+                // client.say(targetChannel,`${multiLink}`);
+                return multiLink;
             }
-            });
-            // client.say(targetChannel,`${multiLink}`);
-            return multiLink;
+        }
+        else { 
+            console.log('Topic does not have !multi and @ in title');
         }
     }
-    else { 
-        console.log('Topic does not have !multi and @ in title');
+    catch(error) { 
+        console.log(`Error reading title. Is the channel streaming?`);
     }
 }
 module.exports.getMultiLink = getMultiLink; 
